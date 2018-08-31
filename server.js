b5a680baea06f9a44d622c4def30614a07b71b6c
WhoisJohnKim <johnkim9417@gmail.com>
2018-08-30 23:53:07 -0500

    Finished the basic html for both home and survey. Added basic app.get pathways. Able to load home page and travel to survey page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -13,6 +13,63 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/survey", function(req, res){
+// Friends (DATA)
+// Start with initial default friend options
+// =============================================================
+var friends = [
+    {
+        name: "John Kim",
+        picture: "",
+        scores: [
+            1,
+            3,
+            2,
+            5,
+            3,
+            2,
+            1,
+            2,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Sean Angle",
+        picture: "",
+        scores: [
+            3,
+            2,
+            4,
+            3,
+            2,
+            4,
+            3,
+            2,
+            1,
+            4
+        ]
+    }
+];
 
+
+// Routes
+// =============================================================
+
+// Basic routes that send the user first to the AJAX page
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+app.get("/api/friends", function(req, res){
+    return res.json(friends);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
